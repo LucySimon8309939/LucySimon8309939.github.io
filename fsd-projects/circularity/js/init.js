07fbf349c0cf80e49145fd212a400c0b9ec4815a
LucySimon8309939 <lms8309939@lpssonline.com>
2026-03-31 16:54:02 +0000

    todo 2 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -15,61 +15,42 @@ var init = function (window) {
         window.opspark.game = {};
         var game = window.opspark.game;
         
-        ///////////////////
-        // PROGRAM SETUP //
-        ///////////////////
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM SETUP ////////////////////////////
+        ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-        var circle
-        var circles = []
-
-
+var circle
+var circles = []
+      
         // TODO 2 : Create a function that draws a circle 
         function drawCircle() {
-            circle = draw.randomCircleArea(canvas,true,true,"#999",2);
-                physikz.addRandomVelocity(circle,canvas,2,2);
-                view.addChild(circle);
-                circles.push(circle);
-            
+ circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+              physikz.addRandomVelocity(circle, canvas, 2, 2);
+              view.addChild(circle);
+              circles.push(circle);
         }
+       
 
+        // TODO 3 / 7 : Call the drawCircle() function 
 
-        // TODO 3 : Call the drawCircle() function
-       drawCircle()
-       drawCircle()
-       drawCircle()
-       drawCircle()
-       drawCircle()
-        // TODO 7 : Use a loop to create multiple circles
-
-
-
-
-        ///////////////////
-        // PROGRAM LOGIC //
-        ///////////////////
+        
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM LOGIC ////////////////////////////
+        ////////////////////////////////////////////////////////////
         
         /* 
-        This Function is called 60 times/second, producing 60 frames/second.
+        This Function is called 60 times/second producing 60 frames/second.
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
         function update() {
-            // TODO 4 : Update the position of each circle using physikz.updatePosition()
-         physikz.updatePosition(circles[0])
-         physikz.updateposition(circles[1])
-         physikz.updatePosition(circles[2])
-         physikz.updatePosition(circles[3])
-         physikz.updatePosition(circles[4])
-            
-            // TODO 5 : Call game.checkCirclePosition() on your circles
-          game.checkCirclePosition(circles[0])
-          game.checkCirclePosition(circles[1])
-          game.checkCirclePosition(circles[2])
-          game.checkCirclePosition(circles[3])
-          game.checkCirclePosition(circles[4])
+            // TODO 4 : Update the circle's position //
 
-            // TODO 8 / TODO 9 : Iterate over the array
+                      
+            // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
+
+                       // TODO 9 : Iterate over the array
            
             
         }
@@ -79,17 +60,16 @@ var init = function (window) {
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
+
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
-            }
-            
+                      if(circle.x > canvas.width){
+  circle.x = 0
+}
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
-
-
+           
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
@@ -109,9 +89,10 @@ var init = function (window) {
     }
 };
 
-// DO NOT REMOVE THIS CODE //////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
 }
+
